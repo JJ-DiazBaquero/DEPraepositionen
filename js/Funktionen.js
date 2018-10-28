2d1b69c39c43d6173f23ca3a40462839c0a6a910
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2018-10-28 15:52:17 +0100

    Kraftsimulation in d3 gefügt wird, um Layout zu verbessen ColorBrewers Farben gefügt werden Schrift Robot gefügt wird
    Added force simulation in d3 to improve layout
    Added colors from ColorBrewer to make it prettier
    Added Font Roboto

---
+++ js/Funktionen.js
@@ -0,0 +1,35 @@
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
+function diff(A, B) {
+    return A.filter(function (a) {
+        return B.indexOf(a) == -1;
+    });
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
--- js/game.js
+++ js/game.js
@@ -4,53 +4,24 @@ var svgGraph = d3.select("#graph"),
     width = svgGraph.node().getBoundingClientRect().width - margin.left - margin.right,
     height = svgGraph.node().getBoundingClientRect().height - margin.top - margin.bottom;
 
-function getRandomSubarray(arr, size) {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
-    while (i--) {
-        index = Math.floor((i + 1) * Math.random());
-        temp = shuffled[index];
-        shuffled[index] = shuffled[i];
-        shuffled[i] = temp;
-    }
-    return shuffled.slice(0, size);
-}
-
-function diff(A, B) {
-    return A.filter(function (a) {
-        return B.indexOf(a) == -1;
-    });
-}
-
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
 d3.csv("data/verbenListe.csv", function(error, data) {
     if (error) throw error;
 
     var hintergrund = svgGraph.append("g");
 
     hintergrund.append("rect")
+        .attr("width", width+ margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("fill", "#ece2f0");
+
+    hintergrund.append("rect")
         .attr("x", margin.left)
         .attr("y", margin.top)
         .attr("width", width)
         .attr("height", height)
-        .style("fill", "blue");
+        .style("stroke","black")
+        .style("stroke-width", "3")
+        .style("fill", "#00f0");
 
     // 4 bucktabe mit 32vw ist genug gross
     // 3 bucktabe mit 50vw ist zu gross
@@ -60,7 +31,9 @@ d3.csv("data/verbenListe.csv", function(error, data) {
         .attr("x", width/2)
         .attr("y", height - margin.bottom)
         .attr("text-anchor", "middle")
-        .attr("font-size", "32vw");
+        .attr("font-size", "32vw")
+        .style("fill", "#a6bddb")
+        .style("opacity", 1);
 
 
     var tree = d3.nest()
@@ -103,6 +76,7 @@ d3.csv("data/verbenListe.csv", function(error, data) {
         .attr("y", height)
         .attr("font-size", "8vw")
         .attr("text-anchor", "end")
+        .style("fill", "#a6bddb")
         .text(aktuelleGefunden +"/" + insgesamt)
 
     var gFehlerZaehler = svgGraph.append("g");
@@ -111,49 +85,74 @@ d3.csv("data/verbenListe.csv", function(error, data) {
         .attr("x", margin.left)
         .attr("y", height)
         .attr("font-size", "8vw")
+        .style("fill", "#a6bddb")
         .attr("text-anchor", "start")
         .text(fehler);
 
     var x = d3.scaleLinear()
         .range([0, width])
-        .domain([0,100]);
+        .domain([0,1]);
 
     var y = d3.scaleLinear()
         .range([0, height])
-        .domain([0,100]);
+        .domain([0,1]);
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
     var gWorte = svgGraph.append("g")
-        .attr("transform","translate("+margin.left+","+margin.top+")")
         .selectAll("g")
         .data(worteListe);
 
     gWorteEnter = gWorte.enter()
         .append("g")
         .attr("class",function(d,i){return "g "+i;})
-        .attr("transform", function(d,i){return "translate(" + x(i*10) + "," + height/2 + ")"})
+/*
+        .attr("transform", function(d,i){
+            // Upper elipse
+            var xPos = 0.5 - Math.cos(i/10*Math.PI)/2.5;
+            var yPos = 0.5 - Math.sin(i/10*Math.PI)/2.5;
+            return "translate(" + x(xPos) + "," + y(yPos) + ")"
+        })
+*/
         .on("click", function(d){
             if(d.type == "F"){
                 d3.select(this).transition()
-                    .duration(1000)
+                    .duration(500)
                     .ease(d3.easeLinear)
-                    .style("opacity", 0);
+                    .style("opacity", 0)
+                    .remove();
                 aktuelleGefunden += 1;
                 d3.select("#Punktzahl")
-                    .text(aktuelleGefunden+"/"+insgesamt)
+                    .text(aktuelleGefunden+"/"+insgesamt);
+
+                worteListe  = worteListe.filter(worte => worte.wort != d.wort)
+                simulation.nodes(worteListe)
+                simulation.restart();
             }if(d.type == "R"){
                 fehler += 1;
+                d3.select(this).transition()
+                    .duration(80)
+                    .attr("transform", "translate(8)")
+                    .transition()
+                    .duration(80)
+                    .attr("transform", "translate(0)");
                 d3.select("#Fehlerzahl")
-                    .text(fehler)
+                    .text(fehler);
+                simulation.restart();
             }
         });
 
     gWorteEnter.append("circle")
         .attr("r",60)
-        .attr("class",function(d,i){return "circle "+i;});
+        .attr("class",function(d,i){return "circle "+i;})
+        .style("fill", "#1c9099")
+        .style("opacity", 0.95);
 
     gWorteEnter.append("text")
         .attr("text-anchor", "middle")
         .text(function(d){return d.wort.replace(/sich|\(sich\)/g, "");})
+        .style("font-family", "'Roboto', sans-serif")
+        .style("font-weight", "bold")
         .attr("fill", "white");
 
     gWorteEnter.append("text")
@@ -162,8 +161,28 @@ d3.csv("data/verbenListe.csv", function(error, data) {
             if(d.wort.includes("(sich)")){return "(sich)";}
             if(d.wort.includes("sich")){return "sich";}
             return "";})
+        .style("font-family", "'Roboto', sans-serif")
+        .style("font-weight", "bold")
         .attr("dy", "-1em")
         .attr("fill", "white")
+
+    var ticked = function() {
+        /*aktualisieren die Simulation*/
+        gWorteEnter.selectAll("circle")
+            .attr("cx", function(d) { return d.x = Math.max(60+margin.left, Math.min(width - margin.right - 120, d.x)); })
+            .attr("cy", function(d) { return d.y = Math.max(60+margin.top, Math.min(height - margin.bottom - 120, d.y)); });
+
+        gWorteEnter.selectAll("text")
+            .attr("x", function(d) { return d.x })
+            .attr("y", function(d) { return d.y });
+    };
+
+    var simulation = d3.forceSimulation(worteListe)
+        .force("x", d3.forceX(x(0.5)).strength(0.1))
+        .force("y", d3.forceY(y(0.5)).strength(0.1))
+        .force("collide", d3.forceCollide(60))
+        .force("repulsion",d3.forceManyBody().strength(-2500))
+        .on("tick",ticked);
 
 
 });
