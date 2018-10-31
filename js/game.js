e85c4286b2ef44ab105805ae83dae471a23a904b
Juan José Díaz <jj.diaz1067@uniandes.edu.co>
2018-10-31 19:09:03 +0100

    Added repeat level button
    Nochmalknopf gefuegt werde

--- js/game.js
+++ js/game.js
@@ -39,8 +39,6 @@ d3.tsv("data/verbenListe.txt", function(error, data) {
         .key(function(d) { return d.Präposition; })
         .map(data);
 
-    console.log(tree)
-
     //praepositionenArr = tree.map(function(d){ return d.key})
     praepositionenArr = tree.keys();
 
@@ -57,17 +55,10 @@ d3.tsv("data/verbenListe.txt", function(error, data) {
     var richtigeWorte = getRandomSubarray(richtigeWorteListe, Math.floor(nummerWorteBild*prozentRichtig));
     var worteListe = data.map(function(d){ return d});
     var falscheWorteList = diff(worteListe, richtigeWorteListe);
-
-    console.log(worteListe)
-    console.log(richtigeWorteListe)
-    console.log("oeoeo")
-    console.log(falscheWorteList)
     var falscheWorte = getRandomSubarray(falscheWorteList, Math.ceil(nummerWorteBild*(1-prozentRichtig)));
 
     worteListe = richtigeWorte.map(function(d){ return {wort: d.Verb, type: "R", beispiel: d.Beispiel, kasus: d.Kasus}});
     worteListe = worteListe.concat(falscheWorte.map(function(d){ return {wort: d.Verb, type: "F", beispiel: d.Beispiel, kasus: d.Kasus}}));
-    console.log(worteListe);
-
     worteListe = shuffle(worteListe);
 
     // die Punkt Zaehler
@@ -106,159 +97,158 @@ d3.tsv("data/verbenListe.txt", function(error, data) {
 
     var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    var gWorte = svgGraph.append("g")
-        .selectAll("g")
-        .data(worteListe);
+    var gWorte;
 
-    gWorteEnter = gWorte.enter()
-        .append("g")
-        .attr("class",function(d,i){return "g "+i;})
-/*
-        .attr("transform", function(d,i){
-            // Upper elipse
-            var xPos = 0.5 - Math.cos(i/10*Math.PI)/2.5;
-            var yPos = 0.5 - Math.sin(i/10*Math.PI)/2.5;
-            return "translate(" + x(xPos) + "," + y(yPos) + ")"
-        })
-*/
-        .on("click", function(d){
-            if(d.type == "F"){
-                d3.select(this).transition()
-                    .duration(500)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 0)
-                    .remove();
-                aktuelleGefunden += 1;
-                d3.select("#Punktzahl")
-                    .text(aktuelleGefunden+"/"+insgesamt);
+    function buildGame(){
+        gWorte = svgGraph.append("g")
+            .selectAll("g")
+            .data(worteListe);
 
-                var siegZiehen = svgGraph.append("text")
-                    .attr("fill", "#3c763d")
-                    .attr("class","far")
-                    .attr("font-size", "8vw")
-                    .attr("dy", "-1em")
-                    .attr("dx", "-1em")
-                    .attr("x", width)
-                    .attr("y", height)
-                    .text("\uf118");
+        gWorteEnter = gWorte.enter()
+            .append("g")
+            .attr("class",function(d,i){return "g "+i;})
+            .on("click", function(d){
+                if(d.type == "F"){
+                    d3.select(this).transition()
+                        .duration(500)
+                        .ease(d3.easeLinear)
+                        .style("opacity", 0)
+                        .remove();
+                    aktuelleGefunden += 1;
+                    d3.select("#Punktzahl")
+                        .text(aktuelleGefunden+"/"+insgesamt);
 
-                siegZiehen.transition()
-                    .duration(1000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 0)
-                    .attr("transform", "translate(0,"+(-height/2)+")")
-                    .remove();
+                    var siegZiehen = svgGraph.append("text")
+                        .attr("fill", "#3c763d")
+                        .attr("class","far")
+                        .attr("font-size", "8vw")
+                        .attr("dy", "-1em")
+                        .attr("dx", "-1em")
+                        .attr("x", width)
+                        .attr("y", height)
+                        .text("\uf118");
 
-                worteListe  = worteListe.filter(worte => worte.wort != d.wort);
-                simulation.nodes(worteListe);
-                simulation.force("repulsion").strength(simulation.force("repulsion").strength()-100);
-                simulation.restart();
+                    siegZiehen.transition()
+                        .duration(1000)
+                        .ease(d3.easeLinear)
+                        .style("opacity", 0)
+                        .attr("transform", "translate(0,"+(-height/2)+")")
+                        .remove();
 
-                if(aktuelleGefunden == insgesamt){
-                    level1Beendet();
+                    worteListe  = worteListe.filter(worte => worte.wort != d.wort);
+                    simulation.nodes(worteListe);
+                    simulation.force("repulsion").strength(simulation.force("repulsion").strength()-100);
+                    simulation.restart();
+
+                    if(aktuelleGefunden == insgesamt){
+                        praeposition.remove();
+                        gWorte.remove();
+                        gWorteEnter.remove();
+
+                        level1Beendet();
+                    }
+
+                }if(d.type == "R"){
+                    fehler += 1;
+                    d3.select(this).transition()
+                        .duration(80)
+                        .attr("transform", "translate(8)")
+                        .transition()
+                        .duration(80)
+                        .attr("transform", "translate(0)");
+                    d3.select("#Fehlerzahl")
+                        .text(fehler);
+
+                    // Trauriges Gesicht
+                    var fehelerZiehen = svgGraph.append("text")
+                        .attr("fill", "#a94442")
+                        .attr("class","far")
+                        .attr("font-size", "8vw")
+                        .attr("dy", "-1em")
+                        .attr("dx", "1em")
+                        .attr("x", 0)
+                        .attr("y", height)
+                        .text("\uf119");
+
+                    fehelerZiehen.transition()
+                        .duration(2000)
+                        .ease(d3.easeLinear)
+                        .style("opacity", 0)
+                        .attr("transform", "translate(0,"+(-height/2)+")")
+                        .remove();
+
+                    if(!d3.select("#zBLevel1").empty()){
+                        d3.select("#zBLevel1").remove()
+                    }
+
+                    // Zeihgen Beispiel
+                    var beispiel = svgGraph.append("text")
+                        .attr("fill", "#a94442")
+                        .attr("id", "zBLevel1")
+                        .attr("font-size", "3vw")
+                        .attr("x", (width+margin.right+margin.left)/2)
+                        .attr("text-anchor", "middle")
+                        .attr("y", height)
+                        .text("z.B."+ d.beispiel);
+
+                    beispiel.transition()
+                        .duration(8000)
+                        .ease(d3.easeLinear)
+                        .style("opacity", 0)
+                        .remove();
+
+                    simulation.restart();
                 }
+            });
 
-            }if(d.type == "R"){
-                fehler += 1;
-                d3.select(this).transition()
-                    .duration(80)
-                    .attr("transform", "translate(8)")
-                    .transition()
-                    .duration(80)
-                    .attr("transform", "translate(0)");
-                d3.select("#Fehlerzahl")
-                    .text(fehler);
+        gWorteEnter.append("circle")
+            .attr("r",60)
+            .attr("class",function(d,i){return "circle "+i;})
+            .style("fill", "#1c9099")
+            .style("opacity", 0.7);
 
-                // Trauriges Gesicht
-                var fehelerZiehen = svgGraph.append("text")
-                    .attr("fill", "#a94442")
-                    .attr("class","far")
-                    .attr("font-size", "8vw")
-                    .attr("dy", "-1em")
-                    .attr("dx", "1em")
-                    .attr("x", 0)
-                    .attr("y", height)
-                    .text("\uf119");
+        gWorteEnter.append("text")
+            .attr("text-anchor", "middle")
+            .text(function(d){return d.wort.replace(/sich|\(sich\)/g, "");})
+            .style("font-family", "'Roboto', sans-serif")
+            .attr("font-size", "1.5vw")
+            .style("font-weight", "bold")
+            .attr("fill", "black");
 
-                fehelerZiehen.transition()
-                    .duration(2000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 0)
-                    .attr("transform", "translate(0,"+(-height/2)+")")
-                    .remove();
+        gWorteEnter.append("text")
+            .attr("text-anchor", "middle")
+            .text(function(d){
+                if(d.wort.includes("(sich)")){return "(sich)";}
+                if(d.wort.includes("sich")){return "sich";}
+                return "";})
+            .style("font-family", "'Roboto', sans-serif")
+            .attr("font-size", "1.5vw")
+            .style("font-weight", "bold")
+            .attr("dy", "-1em")
+            .attr("fill", "black")
 
-                if(!d3.select("#zBLevel1").empty()){
-                    d3.select("#zBLevel1").remove()
-                }
+        var ticked = function() {
+            /*aktualisieren die Simulation*/
+            gWorteEnter.selectAll("circle")
+                .attr("cx", function(d) { return d.x = Math.max(60+margin.left, Math.min(width - margin.right - 120, d.x)); })
+                .attr("cy", function(d) { return d.y = Math.max(60+margin.top, Math.min(height - margin.bottom - 120, d.y)); });
 
-                // Zeihgen Beispiel
-                var beispiel = svgGraph.append("text")
-                    .attr("fill", "#a94442")
-                    .attr("id", "zBLevel1")
-                    .attr("font-size", "3vw")
-                    .attr("x", (width+margin.right+margin.left)/2)
-                    .attr("text-anchor", "middle")
-                    .attr("y", height)
-                    .text("z.B."+ d.beispiel);
+            gWorteEnter.selectAll("text")
+                .attr("x", function(d) { return d.x })
+                .attr("y", function(d) { return d.y });
+        };
 
-                beispiel.transition()
-                    .duration(8000)
-                    .ease(d3.easeLinear)
-                    .style("opacity", 0)
-                    .remove();
-
-                simulation.restart();
-            }
-        });
-
-    gWorteEnter.append("circle")
-        .attr("r",60)
-        .attr("class",function(d,i){return "circle "+i;})
-        .style("fill", "#1c9099")
-        .style("opacity", 0.7);
-
-    gWorteEnter.append("text")
-        .attr("text-anchor", "middle")
-        .text(function(d){return d.wort.replace(/sich|\(sich\)/g, "");})
-        .style("font-family", "'Roboto', sans-serif")
-        .attr("font-size", "1.5vw")
-        .style("font-weight", "bold")
-        .attr("fill", "black");
-
-    gWorteEnter.append("text")
-        .attr("text-anchor", "middle")
-        .text(function(d){
-            if(d.wort.includes("(sich)")){return "(sich)";}
-            if(d.wort.includes("sich")){return "sich";}
-            return "";})
-        .style("font-family", "'Roboto', sans-serif")
-        .attr("font-size", "1.5vw")
-        .style("font-weight", "bold")
-        .attr("dy", "-1em")
-        .attr("fill", "black")
-
-    var ticked = function() {
-        /*aktualisieren die Simulation*/
-        gWorteEnter.selectAll("circle")
-            .attr("cx", function(d) { return d.x = Math.max(60+margin.left, Math.min(width - margin.right - 120, d.x)); })
-            .attr("cy", function(d) { return d.y = Math.max(60+margin.top, Math.min(height - margin.bottom - 120, d.y)); });
-
-        gWorteEnter.selectAll("text")
-            .attr("x", function(d) { return d.x })
-            .attr("y", function(d) { return d.y });
+        var simulation = d3.forceSimulation(worteListe)
+            .force("x", d3.forceX(x(0.5)).strength(0.05))
+            .force("y", d3.forceY(y(0.5)).strength(0.05))
+            .force("collide", d3.forceCollide(60))
+            .force("repulsion",d3.forceManyBody().strength(-1300))
+            .on("tick",ticked);
     };
 
-    var simulation = d3.forceSimulation(worteListe)
-        .force("x", d3.forceX(x(0.5)).strength(0.05))
-        .force("y", d3.forceY(y(0.5)).strength(0.05))
-        .force("collide", d3.forceCollide(60))
-        .force("repulsion",d3.forceManyBody().strength(-1300))
-        .on("tick",ticked);
+    buildGame();
 
     var level1Beendet = function(){
-
-        praeposition.remove();
-        gWorteEnter.remove();
 
         var gEndLevel = svgGraph.append("g");
         var titel = gEndLevel.append("text")
@@ -276,7 +266,40 @@ d3.tsv("data/verbenListe.txt", function(error, data) {
             .attr("font-size", "8vw")
             .attr("x", width/6)
             .attr("y", 2*height/3)
-            .text("\uf01e");
+            .text("\uf01e")
+            .on("click", function(){
+                gEndLevel.remove();
+                gewaehltPraep = Math.random() * praepositionenArr.length | 0;
+
+                praeposition = hintergrund.append("text")
+                    .attr("x", width/2)
+                    .attr("y", 3*height/4)
+                    .attr("text-anchor", "middle")
+                    .attr("font-size", "32vw")
+                    .style("fill", "#a6bddb")
+                    .style("opacity", 1)
+                    .text(praepositionenArr[gewaehltPraep]);
+
+                richtigeWorteListe = tree["$"+praepositionenArr[gewaehltPraep]].map(function(d){return d});
+                richtigeWorte = getRandomSubarray(richtigeWorteListe, Math.floor(nummerWorteBild*prozentRichtig));
+                worteListe = data.map(function(d){ return d});
+                falscheWorteList = diff(worteListe, richtigeWorteListe);
+                falscheWorte = getRandomSubarray(falscheWorteList, Math.ceil(nummerWorteBild*(1-prozentRichtig)));
+
+                worteListe = richtigeWorte.map(function(d){ return {wort: d.Verb, type: "R", beispiel: d.Beispiel, kasus: d.Kasus}});
+                worteListe = worteListe.concat(falscheWorte.map(function(d){ return {wort: d.Verb, type: "F", beispiel: d.Beispiel, kasus: d.Kasus}}));
+                worteListe = shuffle(worteListe);
+
+                aktuelleGefunden = 0;
+                insgesamt = falscheWorte.length;
+
+                gZaehler.select("#Punktzahl")
+                    .text(aktuelleGefunden +"/" + insgesamt);
+                gFehlerZaehler.select("#Fehlerzahl")
+                    .text(fehler);
+
+                buildGame();
+        });
 
         var naechste = gEndLevel.append("text")
             .attr("fill", "black")
@@ -286,7 +309,7 @@ d3.tsv("data/verbenListe.txt", function(error, data) {
             .attr("y", 2*height/3)
             .text("\uf061");
 
-        var beispiel = svgGraph.append("text")
+        var beispiel = gEndLevel.append("text")
             .attr("id", "zBLevel1")
             .attr("font-size", "3vw")
             .style("font-family", "'Roboto', sans-serif")
